215984b442814b8650420c4f05e26d27f6b4cd70
Stephen Jones <stephen.jacob.jones@gmail.com>
2014-05-03 12:39:28 -0400

    Initial Commit

---
+++ SpaceGame/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SpaceGame
+//
+
+//  Copyright (c) 2014 Bits N Grits. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ SpaceGame/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SpaceGame
+//
+
+//  Copyright (c) 2014 Bits N Grits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
